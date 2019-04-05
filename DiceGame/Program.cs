5bd4ea40dc57a80dbfec887be6bce7eb102c8848
LtCupcakes541 <vinnywilliams13@gmail.com>
2019-04-04 21:59:35 -0500

    Add project files.

---
+++ DiceGame/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static Random rdm = new Random();
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Please enter your name");
+            string PlayerName = Console.ReadLine();
+            Console.WriteLine("Hello, " + PlayerName);
+            Console.WriteLine("Your opponent's name is Jork");
+            do { 
+                Console.WriteLine("Press any key to roll the dice");
+                Console.ReadLine();
+                int PlayerDice = rdm.Next(1, 7);
+                int OppDice = rdm.Next(1, 7);
+                Console.WriteLine("You rolled a " + PlayerDice);
+                Console.WriteLine("Jork rolled a " + OppDice);
+
+                if (PlayerDice > OppDice)
+                {
+                    Console.WriteLine("You win!");
+                }
+                else if (PlayerDice < OppDice)
+                {
+                    Console.WriteLine("Jork wins!");
+                }
+                else
+                {
+                    Console.WriteLine("You tied!");
+                }
+                Console.WriteLine("Would you like to play again?");
+                Answer:
+                string Again = Console.ReadLine();
+                if (Again == "no") {
+                    break;
+                }
+                else if(Again == "yes")
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Please say yes or no");
+                    goto Answer;
+                }
+            } while(true);
+        }
+    }
+}
